0fdd1fbf99d0d232f97b5db0e5f57fbab76e5f83
i15317 <miyake.kentaro0815@mokkuriko.com>
2021-03-17 03:25:42 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-
-docker build ./meido-api --tag notchman/meido-flask:latest --build-arg flask_url=$1
-
+docker build ./flask --tag notchman/meido-flask:latest --build-arg flask_url=$1
 docker push notchman/meido-flask:latest
-
